20fb23fbc9d606466322bb4b9fe3fb555e570097
valdefreaks <valdefreaks92@gmail.com>
2020-12-10 17:26:39 -0600

    stable version before slider implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'farcode';
+  navigationBackground: boolean;
+  doSomethingOnWindowScroll($event) {
+    let scrollOffset = $event.children[0].scrollTop;
+    if(scrollOffset >500){
+      this.navigationBackground = true;
+    } else{
+      this.navigationBackground = false;
+    }
+    console.log("window scroll: ", scrollOffset);
+  }
 }
