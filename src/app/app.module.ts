78b0c9cfa103a0f7768d2c8007e797d874f0a973
valdefreaks <valdefreaks92@gmail.com>
2020-12-13 19:39:11 -0600

    v1.0 estable before angular animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {IvyCarouselModule} from 'angular-responsive-carousel';
+import { IvyCarouselModule } from 'angular-responsive-carousel';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    IvyCarouselModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		IvyCarouselModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
